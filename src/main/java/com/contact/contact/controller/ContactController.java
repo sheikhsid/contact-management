3840de85778a83345be7f261af92585cc95071b6
Sheikh Saad <sheikhsaadsd@gmail.com>
2023-05-26 12:20:44 +0200

    styling in UI

--- src/main/java/com/contact/contact/controller/ContactController.java
+++ src/main/java/com/contact/contact/controller/ContactController.java
@@ -2,7 +2,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class ContactController {
 
     @GetMapping("/")
     public String home() {
