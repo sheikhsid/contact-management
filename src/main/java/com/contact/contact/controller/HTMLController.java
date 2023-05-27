bbfd1f343ef50ef9508f454761549035ddc4fe85
Sheikh Saad <sheikhsaadsd@gmail.com>
2023-05-27 19:44:51 +0200

    HTMLController

---
+++ src/main/java/com/contact/contact/controller/HTMLController.java
@@ -0,0 +1,25 @@
+package com.contact.contact.controller;
+
+import com.contact.contact.model.ContactModel;
+import com.contact.contact.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HTMLController {
+
+    private final ContactRepository contactRepository;
+
+    @Autowired
+    public HTMLController(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
+    @PostMapping("/saveContact")
+    public String saveContact(@ModelAttribute ContactModel contact) {
+        contactRepository.save(contact);
+        return "redirect:/success.html";
+    }
+}
