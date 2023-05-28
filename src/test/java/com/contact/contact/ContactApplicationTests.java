5cbcde4139bb2578a1c3a5b8d184b313a6116ea7
Sheikh Saad <sheikhsaadsd@gmail.com>
2023-05-28 23:23:21 +0200

    ContactApplicationTests Updates

--- src/test/java/com/contact/contact/ContactApplicationTests.java
+++ src/test/java/com/contact/contact/ContactApplicationTests.java
@@ -1,14 +1,13 @@
 package com.contact.contact;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(JUnit4.class)
 @SpringBootTest
-public class ContactApplicationTests {
-    @Test
-    public void contextLoads() {
-    }
+class ContactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
 }
