b8c4ccd157fd37199c1238744b2ac5bc640fbc95
Sheikh Saad <sheikhsaadsd@gmail.com>
2023-05-28 10:31:26 +0200

    ContactDto

--- src/main/java/com/contact/contact/controller/HTMLController.java
+++ src/main/java/com/contact/contact/controller/HTMLController.java
@@ -4,11 +4,8 @@ import com.contact.contact.model.ContactDto;
 import com.contact.contact.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.validation.Valid;
 
 @Controller
 public class HTMLController {
@@ -21,15 +18,8 @@ public class HTMLController {
     }
 
     @PostMapping("/saveContact")
-    public String saveContact(@ModelAttribute @Valid ContactDto contactDto, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            // Handle validation errors, e.g., return to the form with error messages
-            return "contact-form";
-        }
-
-        // Perform additional sanitization or validation checks if needed
-
-        contactRepository.save(contactDto);
+    public String saveContact(@ModelAttribute ContactDto contact) {
+        contactRepository.save(contact);
         return "redirect:/";
     }
 }
