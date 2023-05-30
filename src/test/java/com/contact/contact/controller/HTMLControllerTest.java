79ec8768616d645751a65b64a6a1ed0d75e98e38
Sheikh Saad <sheikhsaadsd@gmail.com>
2023-05-30 23:25:11 +0200

    MockMvc

--- src/test/java/com/contact/contact/controller/HTMLControllerTest.java
+++ src/test/java/com/contact/contact/controller/HTMLControllerTest.java
@@ -1,41 +1,46 @@
 package com.contact.contact.controller;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import com.contact.contact.model.ContactDto;
 import com.contact.contact.repository.ContactRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 public class HTMLControllerTest {
 
     private MockMvc mockMvc;
-    private HTMLController htmlController;
+
+    @Mock
     private ContactRepository contactRepository;
 
-    @Before
+    private HTMLController htmlController;
+
+    @SuppressWarnings("deprecation")
+	@Before
     public void setUp() {
-        contactRepository = mock(ContactRepository.class);
+        MockitoAnnotations.initMocks(this);
         htmlController = new HTMLController(contactRepository);
         mockMvc = MockMvcBuilders.standaloneSetup(htmlController).build();
     }
 
     @Test
     public void testSaveContact() throws Exception {
-        ContactDto contact = new ContactDto(1L, "Sheikh Saad", null, null, "sheikh@domain.com");
+        ContactDto contactDto = new ContactDto();
+        contactDto.setName("Sheikh");
+        contactDto.setEmail("sheikh@example.com");
 
-        mockMvc.perform(post("/saveContact")
-                .flashAttr("contactDto", contact))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/"));
+        mockMvc.perform(MockMvcRequestBuilders.post("/saveContact")
+                .flashAttr("contactDto", contactDto))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/"));
 
-        verify(contactRepository).save(contact);
+        // Verify that the contactRepository.save() method is called
+        Mockito.verify(contactRepository).save(contactDto);
     }
 }
