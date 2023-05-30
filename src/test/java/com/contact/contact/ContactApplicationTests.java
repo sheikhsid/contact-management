4fe78492603b8eae0187f8a6265d4177e90f2d40
Sheikh Saad <sheikhsaadsd@gmail.com>
2023-05-30 12:06:23 +0200

    Assertions

--- src/test/java/com/contact/contact/ContactApplicationTests.java
+++ src/test/java/com/contact/contact/ContactApplicationTests.java
@@ -2,12 +2,13 @@ package com.contact.contact;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Assertions;
 
 @SpringBootTest
 class ContactApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Add your assertion here
+        Assertions.assertTrue(true, "The context should load successfully.");
+    }
 }
