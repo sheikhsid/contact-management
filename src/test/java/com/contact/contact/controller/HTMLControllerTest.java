7123d0b774c85a0f513bdf5ec3471691d30d3fc5
Sheikh Saad <sheikhsaadsd@gmail.com>
2023-05-31 00:13:53 +0200

    HTMLControllerTest update

--- src/test/java/com/contact/contact/controller/HTMLControllerTest.java
+++ src/test/java/com/contact/contact/controller/HTMLControllerTest.java
@@ -5,12 +5,15 @@ import com.contact.contact.repository.ContactRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
 
 public class HTMLControllerTest {
 
@@ -30,17 +33,42 @@ public class HTMLControllerTest {
     }
 
     @Test
-    public void testSaveContact() throws Exception {
+    public void testSaveContact_RedirectsToRoot() throws Exception {
         ContactDto contactDto = new ContactDto();
         contactDto.setName("Sheikh");
-        contactDto.setEmail("sheikh@example.com");
+        contactDto.setEmail("sheikh@domain.com");
 
         mockMvc.perform(MockMvcRequestBuilders.post("/saveContact")
                 .flashAttr("contactDto", contactDto))
                 .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
                 .andExpect(MockMvcResultMatchers.redirectedUrl("/"));
 
-        // Verify that the contactRepository.save() method is called
-        Mockito.verify(contactRepository).save(contactDto);
+        verify(contactRepository).save(contactDto);
     }
+
+    @Test
+    public void testSaveContact_MissingName() throws Exception {
+        ContactDto contactDto = new ContactDto();
+        contactDto.setEmail("sheikh@domain.com");
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/saveContact")
+                .flashAttr("contactDto", contactDto))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
+
+        verify(contactRepository, times(1)).save(eq(contactDto));
+    }
+
+    @Test
+    public void testSaveContact_MissingEmail() throws Exception {
+        ContactDto contactDto = new ContactDto();
+        contactDto.setName("Sheikh");
+        contactDto.setEmail(null); // Set the email to null
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/saveContact")
+                .flashAttr("contactDto", contactDto))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
+
+        verify(contactRepository, times(1)).save(eq(contactDto));
+    }
+
 }
