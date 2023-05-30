be2140dfe0eb280adb3cf3b9f189b1ce643dd1b5
Sheikh Saad <sheikhsaadsd@gmail.com>
2023-05-30 22:45:05 +0200

    MockMvc

--- src/test/java/com/contact/contact/controller/HTMLControllerTest.java
+++ src/test/java/com/contact/contact/controller/HTMLControllerTest.java
@@ -2,15 +2,21 @@ package com.contact.contact.controller;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.contact.contact.model.ContactDto;
 import com.contact.contact.repository.ContactRepository;
 
 public class HTMLControllerTest {
 
+    private MockMvc mockMvc;
     private HTMLController htmlController;
     private ContactRepository contactRepository;
 
@@ -18,13 +24,17 @@ public class HTMLControllerTest {
     public void setUp() {
         contactRepository = mock(ContactRepository.class);
         htmlController = new HTMLController(contactRepository);
+        mockMvc = MockMvcBuilders.standaloneSetup(htmlController).build();
     }
 
     @Test
-    public void testSaveContact() {
+    public void testSaveContact() throws Exception {
         ContactDto contact = new ContactDto(1L, "Sheikh Saad", null, null, "sheikh@domain.com");
-        @SuppressWarnings("unused")
-		String modelAndView = htmlController.saveContact(contact);
+
+        mockMvc.perform(post("/saveContact")
+                .flashAttr("contactDto", contact))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
 
         verify(contactRepository).save(contact);
     }
