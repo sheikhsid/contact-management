7d63791b781df4553c7e529057812c00f205305f
Sheikh Saad <sheikhsaadsd@gmail.com>
2023-06-08 17:50:14 +0200

    println

--- src/test/java/com/contact/contact/e2e/ContactForm.java
+++ src/test/java/com/contact/contact/e2e/ContactForm.java
@@ -8,36 +8,36 @@ public class ContactForm {
 
     @Given("I am on the contact form page")
     public void i_am_on_the_contact_form_page() {
-        // Implement the code to navigate to the contact form page
+        System.out.println("Navigating to the contact form page");
     }
 
     @When("I enter the name {string}")
     public void i_enter_the_name(String name) {
-        // Implement the code to enter the name in the contact form
+        System.out.println("Entering the name: " + name);
     }
 
     @When("I enter the company {string}")
     public void i_enter_the_company(String company) {
-        // Implement the code to enter the company in the contact form
+        System.out.println("Entering the company: " + company);
     }
 
     @When("I enter the number {string}")
     public void i_enter_the_number(String number) {
-        // Implement the code to enter the number in the contact form
+        System.out.println("Entering the number: " + number);
     }
 
     @When("I enter the email {string}")
     public void i_enter_the_email(String email) {
-        // Implement the code to enter the email in the contact form
+        System.out.println("Entering the email: " + email);
     }
 
     @When("I click the Save button")
     public void i_click_the_save_button() {
-        // Implement the code to click the Save button
+        System.out.println("Clicking the Save button");
     }
 
     @Then("I should see a success message")
     public void i_should_see_a_success_message() {
-        // Implement the code to verify the success message is displayed
+        System.out.println("Verifying the success message");
     }
 }
